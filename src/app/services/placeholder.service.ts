1e3a1b4651fdb235dcc0c0f4730889320ff65feb
Joseph Weber <josephaffeldtw@gmail.com>
2020-11-04 11:36:30 -0300

    Initializing form

--- src/app/services/placeholder.service.ts
+++ src/app/services/placeholder.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Placeholder} from '@angular/compiler/src/i18n/i18n_ast';
+import {ApiListComponent} from '../views/home/api-list/api-list.component';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +21,11 @@ export class PlaceholderService {
     private httpClient: HttpClient
   ) { }
 
-  public getPlaceholders(){
+  public getPlaceholders(): Observable<any>{
     return this.httpClient.get(this.apiUrl);
+  }
+
+  public postPlaceholder(ph: Placeholder): Observable<Placeholder>{
+    return this.httpClient.post<any>(this.apiUrl, ph);
   }
 }
--- src/app/views/home/placeholder-form-dialog/placeholder-form-dialog.component.ts
+++ src/app/views/home/placeholder-form-dialog/placeholder-form-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-placeholder-form-dialog',
@@ -8,11 +9,20 @@ import {MatDialogRef} from '@angular/material/dialog';
 })
 export class PlaceholderFormDialogComponent implements OnInit {
 
+  public forms: FormGroup;
+
   constructor(
+    public fb: FormBuilder,
     public dialogRef: MatDialogRef<PlaceholderFormDialogComponent>
   ) { }
 
   ngOnInit(): void {
+    this.forms = this.fb.group({
+      userId: ['', [Validators.required]],
+      id: ['', Validators.required],
+      title: ['', Validators.required],
+      body: ['', Validators.required]
+    });
   }
 
   cancel(): void {
